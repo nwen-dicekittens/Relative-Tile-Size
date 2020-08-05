d0a90b8064b068b86d3d0f1aa5e3e2a3057f515e
wennx <wennx.96@163.com>
2020-08-04 22:40:45 -0400

    Initial Commit

---
+++ tile-relativesize/scripts/tilerelativesize.js
@@ -0,0 +1,40 @@
+fp = 1;
+
+
+Hooks.on("init", function(filepicker, arg1, arg2)
+{
+    FilePicker.prototype._onDragStart = function(event) 
+    {
+        const li = event.currentTarget;
+        
+        const img = li.querySelector("img");
+    
+        // Get the tile size ratio
+        const tileRatio = (parseInt(li.closest("form").tileSize.value) || canvas.dimensions.size) / canvas.dimensions.size;
+        const tileSize = Math.max(img.naturalWidth, img.naturalHeight) / tileRatio;
+        FilePicker.LAST_TILE_SIZE = tileRatio;
+        const ratio = tileSize / canvas.dimensions.size;
+    
+        // Set drag data
+        const dragData = {
+          type: "Tile",
+          img: li.dataset.path,
+          tileSize: tileSize
+        };
+        event.dataTransfer.setData("text/plain", JSON.stringify(dragData));
+    
+        // Create the drag preview for the image
+        const w = img.naturalWidth * canvas.stage.scale.x * tileRatio / ratio;
+        const h = img.naturalHeight * canvas.stage.scale.y * tileRatio / ratio;
+        const preview = DragDrop.createDragImage(img, w, h);
+        event.dataTransfer.setDragImage(preview, w / 2, h / 2);
+    }
+});
+
+Hooks.on("renderFilePicker", function(filepicker, arg1, arg2)
+{
+    if(filepicker.displayMode === "tiles")
+    {
+        filepicker._element.find(".units")[0].innerText = "%";
+    }
+});
