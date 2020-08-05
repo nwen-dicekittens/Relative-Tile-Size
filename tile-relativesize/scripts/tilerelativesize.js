e889507a5ca6491ce1222670d83c900299ad494c
wennx <wennx.96@163.com>
2020-08-04 23:19:10 -0400

    Initial percentage number corrected.

--- tile-relativesize/scripts/tilerelativesize.js
+++ tile-relativesize/scripts/tilerelativesize.js
@@ -1,28 +1,26 @@
-fp = 1;
+FilePicker.LAST_TILE_SIZE = 100;
 
 
-Hooks.on("init", function(filepicker, arg1, arg2)
-{
-    FilePicker.prototype._onDragStart = function(event) 
-    {
+Hooks.on("init", function (filepicker, arg1, arg2) {
+    FilePicker.prototype._onDragStart = function (event) {
         const li = event.currentTarget;
-        
+
         const img = li.querySelector("img");
-    
+
         // Get the tile size ratio
-        const tileRatio = (parseInt(li.closest("form").tileSize.value) || canvas.dimensions.size) / canvas.dimensions.size;
+        const tileRatio = (parseInt(li.closest("form").tileSize.value) || 100) / 100;
         const tileSize = Math.max(img.naturalWidth, img.naturalHeight) / tileRatio;
         FilePicker.LAST_TILE_SIZE = tileRatio;
         const ratio = tileSize / canvas.dimensions.size;
-    
+
         // Set drag data
         const dragData = {
-          type: "Tile",
-          img: li.dataset.path,
-          tileSize: tileSize
+            type: "Tile",
+            img: li.dataset.path,
+            tileSize: tileSize
         };
         event.dataTransfer.setData("text/plain", JSON.stringify(dragData));
-    
+
         // Create the drag preview for the image
         const w = img.naturalWidth * canvas.stage.scale.x * tileRatio / ratio;
         const h = img.naturalHeight * canvas.stage.scale.y * tileRatio / ratio;
@@ -31,10 +29,8 @@ Hooks.on("init", function(filepicker, arg1, arg2)
     }
 });
 
-Hooks.on("renderFilePicker", function(filepicker, arg1, arg2)
-{
-    if(filepicker.displayMode === "tiles")
-    {
+Hooks.on("renderFilePicker", function (filepicker, arg1, arg2) {
+    if (filepicker.displayMode === "tiles") {
         filepicker._element.find(".units")[0].innerText = "%";
     }
 });
